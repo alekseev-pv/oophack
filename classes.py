0204f44d8f21a3098b312e794de4bf842bb2545c
Igor Shatava <cianoid@yandex.ru>
2021-09-18 12:39:46 +0300

    реализовал методы подсчета поинтов персонажа и тесты к ним

--- classes.py
+++ classes.py
@@ -1,51 +1,87 @@
+from typing import List
+
+
 class Thing:
     """Класс вещи. На вход принимает:
     - имя (name),
-    - процент защиты (defence_percent). Указывается числом от 0 до 1,
+    - процент защиты (defence_percent),
     - количество атаки (attack_points),
     - количество жизней (health_points)."""
     name: str
-    defence_points: float
+    defence_percent: int
     attack_points: float
     health_points: float
 
-    def __init__(self, name, defence_points, attack_points, health_points):
+    def __init__(self, name, defence_percent, attack_points, health_points):
         self.name = name
-        self.defence_points = defence_points
+        self.defence_percent = defence_percent
         self.attack_points = attack_points
         self.health_points = health_points
 
 
 class Person:
+    """Класс персонажа. На вход принимает:
+    - имя (name),
+    - процент защиты (defence_percent),
+    - количество атаки (attack_points),
+    - количество жизней (health_points)."""
     name: str
-    defence_points: float
+    defence_percent: int
     health_points: float
     attack_points: float
+    things: List[Thing]
 
-    def __init__(self, name, defence_points, attack_points, health_points):
+    def __init__(self, name, defence_percent, attack_points, health_points):
         self.name = name
-        self.defence_points = defence_points
+        self.defence_percent = defence_percent
         self.attack_points = attack_points
         self.health_points = health_points
 
     def set_things(self, things):
+        """Устанавливаем список вещей."""
+        self.things = things
+
+    def decrease_health_points(self, attack_damage):
+        """Метод вычитания health_points экземпляра в зависимости от атаки
+        attack_damage."""
         pass
 
-    def decrease_hp(self, foe_attack_points):
-        pass
+    def __total_points(self, attrname):
+        """Вычисление количества поинтов (атрибут передается в attrname)
+        экземпляра."""
+        points = getattr(self, attrname)
+        try:
+            points += sum([getattr(thing, attrname) for thing in self.things])
+        except AttributeError:
+            pass
+
+        return points
+
+    def total_defence_percent(self):
+        """Вычисление количества поинтов защиты."""
+        return self.__total_points('defence_percent')
+
+    def total_attack_points(self):
+        """Вычисление количества поинтов атаки."""
+        return self.__total_points('attack_points')
+
+    def total_health_points(self):
+        """Вычисление количества поинтов здоровья."""
+        return self.__total_points('health_points')
 
 
 class Paladin(Person):
-    def __init__(self, name, defence_points, attack_points, health_points):
-        super().__init__(name, defence_points, attack_points, health_points)
+    """Класс Паладина. Наследуется от Person."""
+    def __init__(self, name, defence_percent, attack_points, health_points):
+        super().__init__(name, defence_percent, attack_points, health_points)
 
         self.health_points *= 2
-        self.defence_points *= 2
+        self.defence_percent *= 2
 
 
 class Warrior(Person):
-    def __init__(self, name, defence_points, attack_points, health_points):
-        super().__init__(name, defence_points, attack_points, health_points)
+    """Класс Воина. Наследуется от Person."""
+    def __init__(self, name, defence_percent, attack_points, health_points):
+        super().__init__(name, defence_percent, attack_points, health_points)
 
         self.attack_points *= 2
-
---
+++ tests.py
@@ -0,0 +1,52 @@
+import unittest
+
+from game import Person, Thing
+
+
+class TestPerson(unittest.TestCase):
+    def setUp(self):
+        things = [
+            Thing(name='Pistol', defence_percent=0, attack_points=50,
+                  health_points=0),
+            Thing(name='Helmet', defence_percent=10, attack_points=0,
+                  health_points=20)
+        ]
+
+        self.person_with_things = Person(
+            name='Person01', defence_percent=10, attack_points=20,
+            health_points=100)
+        self.person_without_things = Person(
+            name='Person02', defence_percent=35, attack_points=10,
+            health_points=100)
+
+        self.person_with_things.set_things(things)
+
+    def test_persons_health_points_with_things(self):
+        call = self.person_with_things.total_health_points()
+        result = 120
+        self.assertEqual(call, result)
+
+    def test_persons_defence_percent_with_things(self):
+        call = self.person_with_things.total_defence_percent()
+        result = 20
+        self.assertEqual(call, result)
+
+    def test_persons_attack_points_with_things(self):
+        call = self.person_with_things.total_attack_points()
+        result = 70
+        self.assertEqual(call, result)
+
+    def test_persons_health_points_without_things(self):
+        call = self.person_without_things.total_health_points()
+        result = 100
+        self.assertEqual(call, result)
+
+    def test_persons_defence_percent_without_things(self):
+        call = self.person_without_things.total_defence_percent()
+        result = 35
+        self.assertEqual(call, result)
+
+    def test_persons_attack_points_without_things(self):
+        call = self.person_without_things.total_attack_points()
+        result = 10
+        self.assertEqual(call, result)
