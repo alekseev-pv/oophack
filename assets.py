6e13478c7ac393f12af93f9a1531d4cec221ffd8
Vlad Nikitin <nikitinv91@gmail.com>
2021-09-18 17:01:38 +0300

    file name change

---
+++ assets.py
@@ -0,0 +1,78 @@
+CHARACTERS = [
+    {
+        'name': 'Darrow O\'Lykos',
+        'hp': 100,
+        'defense': 75,
+        'attack': 100,
+        'fraction': 'gold',
+    },
+    {
+        'name': 'Sevro au Barca',
+        'hp': 80,
+        'defense': 60,
+        'attack': 125,
+        'fraction': 'gold'
+    },
+    {
+        'name': 'Sefi',
+        'hp': 100,
+        'defense': 80,
+        'attack': 90,
+        'fraction': 'obsidian'
+    },
+    {
+        'name': 'Ragnar Volarus',
+        'hp': 150,
+        'defense': 100,
+        'attack': 125,
+        'fraction': 'obsidian'
+    },
+    {
+        'name': 'Volga Fjorgan',
+        'hp': 90,
+        'defense': 70,
+        'attack': 85,
+        'fraction': 'obsidian'
+    },
+    {
+        'name': 'Cassius au Bellona',
+        'hp': 100,
+        'defense': 80,
+        'attack': 90,
+        'fraction': 'gold'
+    },
+
+]
+
+ITEMS = [
+    {
+        'name': 'Razor',
+        'attack': 30,
+        'defense': 5,
+        'durability': 60,
+    },
+    {
+        'name': 'PulseArmor',
+        'attack': 5,
+        'defense': 10,
+        'durability': 90,
+    },
+    {
+        'name': 'SlingBlade',
+        'attack': 20,
+        'defense': 3,
+        'durability': 40,
+    },
+    {
+        'name': 'IonBlade',
+        'attack': 15,
+        'defense': 3,
+        'durability': 50,
+    },
+    {
+        'name': 'GhostCloak',
+        'attack': 3,
+        'defense': 10,
+        'durability': 100,
+    }
+]
