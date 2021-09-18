410b08c86cb3cf2f5f390d4dcfd1794ca0453189
Evgeniya <evgenia@agamova.ru>
2021-09-18 20:46:55 +0300

    реализация мини-проекта 'Игра Арена'

--- oop/oop_hach.py
+++ oop/oop_hach.py
@@ -48,45 +48,50 @@ class Warrior(Person):
         super().__init__(name, hp, 2 * attack, protection)
 
 
-things = []
-for thing in THINGS:
-    things.append(Thing(**thing))
+def main():
+    things = []
+    for thing in THINGS:
+        things.append(Thing(**thing))
 
-persons = []
-count_of_paladins = random.randint(1, 10)
-paladins = random.sample(PERSONS, count_of_paladins)
-count_of_warriors = 10 - count_of_paladins
-for person in PERSONS:
-    if person in paladins:
-        PERSONS.remove(person)
-warriors = random.sample(PERSONS, count_of_warriors)
-for paladin in paladins:
-    persons.append(Paladin(**paladin))
-for warrior in warriors:
-    persons.append(Warrior(**warrior))
+    persons = []
+    count_of_paladins = random.randint(1, 10)
+    paladins = random.sample(PERSONS, count_of_paladins)
+    count_of_warriors = 10 - count_of_paladins
+    for person in PERSONS:
+        if person in paladins:
+            PERSONS.remove(person)
+    warriors = random.sample(PERSONS, count_of_warriors)
+    for paladin in paladins:
+        persons.append(Paladin(**paladin))
+    for warrior in warriors:
+        persons.append(Warrior(**warrior))
 
-for person in persons:
-    count_of_things = random.randint(1, 4)
-    person.set_things(random.sample(things, count_of_things))
+    for person in persons:
+        count_of_things = random.randint(1, 4)
+        person.set_things(random.sample(things, count_of_things))
 
-print(Fore.YELLOW + '   ДА НАЧНЕТСЯ БИТВА!')
-print()
-while len(persons) > 1:
-    fighters = random.sample(persons, 2)
-    print(Fore.CYAN + f'На арену выходят {fighters[0].name} и {fighters[1].name}')
-    while True:
-        first = fighters[0].hit_points(fighters[1])
-        if first <= 0:
-            print(Fore.WHITE + f'{fighters[0].name} проиграл :(')
-            persons.remove(fighters[0])
-            break
-
-        second = fighters[1].hit_points(fighters[0])
-
-        if second <= 0:
-            print(Fore.WHITE + f'{fighters[1].name} проиграл :(')
-            persons.remove(fighters[1])
-            break
+    print(Fore.YELLOW + '   ДА НАЧНЕТСЯ БИТВА!')
     print()
+    while len(persons) > 1:
+        fighters = random.sample(persons, 2)
+        print(Fore.CYAN + f'На арену выходят {fighters[0].name} и {fighters[1].name}')
+        while True:
+            first = fighters[0].hit_points(fighters[1])
+            if first <= 0:
+                print(Fore.WHITE + f'{fighters[0].name} проиграл :(')
+                persons.remove(fighters[0])
+                break
 
-print(Fore.MAGENTA + f'{persons[0].name} победил!')
+            second = fighters[1].hit_points(fighters[0])
+
+            if second <= 0:
+                print(Fore.WHITE + f'{fighters[1].name} проиграл :(')
+                persons.remove(fighters[1])
+                break
+        print()
+
+    print(Fore.MAGENTA + f'{persons[0].name} победил!')
+
+
+if __name__ == "__main__":
+    main()
