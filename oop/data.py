b8fa8a61ac047ec50ee9721be4833937adfd8c07
Evgeniya <evgenia@agamova.ru>
2021-09-18 20:26:02 +0300

    реализация мини-проекта 'Игра Арена'

---
+++ oop/data.py
@@ -0,0 +1,149 @@
+THINGS = [
+    {
+     'name': 'hat',
+     'hp': 5,
+     'attack': 2,
+     'protection': 3
+    },
+    {
+     'name': 'knife',
+     'hp': 5,
+     'attack': 60,
+     'protection': 10
+    },
+    {
+     'name': 'shield',
+     'hp': 10,
+     'attack': 10,
+     'protection': 60
+    },
+    {
+     'name': 'stick',
+     'hp': 10,
+     'attack': 4,
+     'protection': 4
+    },
+    {
+     'name': 'broom',
+     'hp': 5,
+     'attack': 8,
+     'protection': 4
+    },
+    {
+     'name': 'pan',
+     'hp': 5,
+     'attack': 16,
+     'protection': 5
+    }
+]
+
+PERSONS = [
+    {
+     'name': 'Том Круз',
+     'hp': 80,
+     'attack': 32,
+     'protection': 17
+    },
+    {
+     'name': 'Брюс Ли',
+     'hp': 100,
+     'attack': 50,
+     'protection': 25
+    },
+    {
+     'name': 'Владимир Владимирович',
+     'hp': 1000,
+     'attack': 100,
+     'protection': 100
+    },
+    {
+     'name': 'Навальный',
+     'hp': 100,
+     'attack': 40,
+     'protection': 10
+    },
+    {
+     'name': 'Хабиб Нурмагомедов',
+     'hp': 100,
+     'attack': 40,
+     'protection': 30
+    },
+    {
+     'name': 'Конор Макгрегор',
+     'hp': 100,
+     'attack': 35,
+     'protection': 20
+    },
+    {
+     'name': 'Идущий к реке',
+     'hp': 100,
+     'attack': 10,
+     'protection': 10
+    },
+    {
+     'name': 'Рокки Бальбоа',
+     'hp': 100,
+     'attack': 35,
+     'protection': 20
+    },
+    {
+     'name': 'Саб-Зиро',
+     'hp': 100,
+     'attack': 40,
+     'protection': 40
+    },
+    {
+     'name': 'Агент Смит',
+     'hp': 80,
+     'attack': 40,
+     'protection': 30
+    },
+    {
+     'name': 'Нео',
+     'hp': 100,
+     'attack': 30,
+     'protection': 20
+    },
+    {
+     'name': 'Джон Сноу',
+     'hp': 100,
+     'attack': 20,
+     'protection': 20
+    },
+    {
+     'name': 'Тирион Ланнистер',
+     'hp': 80,
+     'attack': 20,
+     'protection': 20
+    },
+    {
+     'name': 'Нед Старк',
+     'hp': 100,
+     'attack': 20,
+     'protection': 10
+    },
+    {
+     'name': 'Терминатор',
+     'hp': 100,
+     'attack': 50,
+     'protection': 40
+    },
+    {
+     'name': 'Джейсон Борн',
+     'hp': 100,
+     'attack': 30,
+     'protection': 20
+    },
+    {
+     'name': 'Доминик Торетто',
+     'hp': 100,
+     'attack': 30,
+     'protection': 20
+    },
+    {
+     'name': 'Коул Янг',
+     'hp': 100,
+     'attack': 40,
+     'protection': 25
+    },
+]
---
+++ oop/oop_hach.py
@@ -0,0 +1,92 @@
+import colorama
+import random
+
+from data import PERSONS, THINGS
+from colorama import Fore
+
+colorama.init()
+
+
+class Base:
+    def __init__(self, name, hp, attack, protection):
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+        self.protection = protection
+
+
+class Thing(Base):
+    pass
+
+
+class Person(Base):
+    def __init__(self, name, hp, attack, protection):
+        super().__init__(name, hp, attack, protection)
+        self.things = []
+        self.final_hp = self.hp + sum(thing.hp for thing in self.things)
+        self.final_protection = self.protection + sum(thing.protection for thing in self.things)
+        self.attack_damage = self.attack + sum(thing.attack for thing in self.things)
+
+    def set_things(self, things):
+        self.things.extend(things)
+
+    def hit_points(self, other):
+        print(Fore.RED + f'{other.name} наносит удар {self.name} на {other.attack_damage} очков урона')
+        hp = self.final_hp - (other.attack_damage - other.attack_damage * self.final_protection * 0.01)
+        self.final_hp = hp
+        print(Fore.GREEN + f'   Уровень жизни {self.name}: {hp}')
+        return hp
+
+
+class Paladin(Person):
+    def __init__(self, name, hp, attack, protection):
+        super().__init__(name, 2 * hp, attack, 2 * protection)
+
+
+class Warrior(Person):
+    def __init__(self, name, hp, attack, protection):
+        super().__init__(name, hp, 2 * attack, protection)
+
+
+things = []
+for thing in THINGS:
+    things.append(Thing(**thing))
+
+persons = []
+count_of_paladins = random.randint(1, 10)
+paladins = random.sample(PERSONS, count_of_paladins)
+count_of_warriors = 10 - count_of_paladins
+for person in PERSONS:
+    if person in paladins:
+        PERSONS.remove(person)
+warriors = random.sample(PERSONS, count_of_warriors)
+for paladin in paladins:
+    persons.append(Paladin(**paladin))
+for warrior in warriors:
+    persons.append(Warrior(**warrior))
+
+for person in persons:
+    count_of_things = random.randint(1, 4)
+    person.set_things(random.sample(things, count_of_things))
+
+print(Fore.YELLOW + '   ДА НАЧНЕТСЯ БИТВА!')
+print()
+while len(persons) > 1:
+    fighters = random.sample(persons, 2)
+    print(Fore.CYAN + f'На арену выходят {fighters[0].name} и {fighters[1].name}')
+    while True:
+        first = fighters[0].hit_points(fighters[1])
+        if first <= 0:
+            print(Fore.WHITE + f'{fighters[0].name} проиграл :(')
+            persons.remove(fighters[0])
+            break
+
+        second = fighters[1].hit_points(fighters[0])
+
+        if second <= 0:
+            print(Fore.WHITE + f'{fighters[1].name} проиграл :(')
+            persons.remove(fighters[1])
+            break
+    print()
+
+print(Fore.MAGENTA + f'{persons[0].name} победил!')
